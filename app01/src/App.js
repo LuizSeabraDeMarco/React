23c1721cdda661c488e33d87222870369f78c2f3
LuizSeabraDeMarco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2023-07-16 20:35:53 -0300

    Começando a prender

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
+import Eu from './componentes/Imgs/eu.jpg'
+
+
 export default function App(){
+    let date = new Date
+    const ano = date.getFullYear()
+    let hora = date.getHours()
+    const canal=()=>{
+      return('Estamos em ')
+    }
+
+    function curso(){
+      return `Agora são ${hora} horas`
+    }
   return(
     <>
-      <p>LFE é dev</p>
-      <br/>
-      <p>Oi</p>
+    <section>
+      <header>
+        <p>{canal()} {ano}</p>
+        <br/>
+        <p>{curso()}</p>
+        </header>
+        <div>
+          <img src={Eu} alt=""></img>
+          <img src="/Img/morte.png"></img>
+        </div>
+      </section>
     </>
   )
 }
